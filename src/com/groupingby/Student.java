4548606e010ebeb58b64d3e1de9b927c75d36370
南京刘德华 <1553173849@qq.com>
2020-01-16 14:06:26 +0800

    groupingby

---
+++ src/com/groupingby/Student.java
@@ -0,0 +1,21 @@
+package com.groupingby;
+
+public class Student {
+    private String name;
+    private int age;
+    private String className;
+    public Student(String name,String className,int age){
+        this.name=name;
+        this.age=age;
+        this.className = className;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getClassName() {
+        return className;
+    }
+}
